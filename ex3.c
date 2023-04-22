ea5e68858caae7a582348ec2aaa20bed677c24fe
Margarita Prokhorova <margarita.prokhorova2017@yandex.ru>
2023-04-22 20:14:06 +0300

    Конечный варинат программы

--- ex3.c
+++ ex3.c
@@ -2,13 +2,17 @@
 
 int main () {
     char word[100];
+    char *word_ukaz; 
     char vowel_letters[6] = {'a', 'e', 'i', 'o', 'u', 'y'};
+    char *vowel_letters_ukaz;
     int vowels = 0, consonants;
     printf ("Введите любое слово: ");
     scanf ("%s", &word);
-    for (int i = 0; word[i]!='\0'; i++) {
+    word_ukaz = word;
+    vowel_letters_ukaz = vowel_letters;
+    for (int i = 0; *(word_ukaz+i)!='\0'; i++) {
         for (int j = 0; j < 6; j++) {
-            if (word[i] == vowel_letters[j]) {
+            if (*(word_ukaz+i) == *(vowel_letters_ukaz+j)) {
                 vowels++;
             }
         }
